8c9e453cad2b8de394b0dc263f3f5533932266b0
Tiago Ribeiro <tiago.astro@gmail.com>
2020-03-04 16:19:49 -0700

    Add producer for the scheduler. (+11 squashed commits) Squashed commits: [74f7826] Update scheduler deployment to not override the setup.sh file. [0c197ae] Add at scheduler. [40f4e64] Deploy watcher and AT Queue [ba0a71a] Fix path to butler data. [1fbcb6e] Fix setup for TeWei's jupyter server [63c325e] Add notebook server for TeWei. [5f76897] Run MT M2 Hexapod in normal mode instead of simulation. [37b1d45] Fixes network name on hexapod. [237469b] Add M2 hexapod deployment. Add notebook server for Andrew Heyer. [10f66c4] Update pointing component deployment. Update kafka producers. [4cf7742] Update AT components to xml 4.7 using the deployment container.

--- AT_CSC/ataos_setup.sh
+++ AT_CSC/ataos_setup.sh
@@ -5,4 +5,4 @@ source ~/miniconda3/bin/activate
 source $OSPL_HOME/release.com
 source /home/saluser/.bashrc
 
-run_atdometrajectory.py
+ataos_csc.py
---
+++ users_jupyter_servers/diag14_notebook_config.py
@@ -0,0 +1,2 @@
+
+c.NotebookApp.token = '4683b245f4231304ff1c0c4595475a55a1d2efd55dea5529'
