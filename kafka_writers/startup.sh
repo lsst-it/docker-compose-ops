79ab7c0521f162465c1b71036bb76aa7db4112da
Michael Reuter <mareuter@lsst.org>
2021-02-04 16:24:04 -0700

    Changes for development kafka producers.

--- kafka_writers/startup.sh
+++ kafka_writers/startup.sh
@@ -4,6 +4,7 @@ export LSST_DDS_ALIGNER=true
 export LSST_DDS_ALIGNEE=Initial
 export OSPL_MASTER_PRIORITY=200
 ospl start
+sleep 30
 run_salkafka_producer.py --broker $BROKER_IP:$BROKER_PORT \
 --registry $REGISTRY_ADDR \
 --partitions  $PARTITIONS \
