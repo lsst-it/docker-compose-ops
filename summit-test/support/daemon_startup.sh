ef4dd6b2e742ce4b6e4b4abf7684d1c168d1b343
Michael Reuter <mareuter@lsst.org>
2021-05-13 12:28:10 -0700

    summit-test: Try different tactic for logs.

--- summit-test/support/daemon_startup.sh
+++ summit-test/support/daemon_startup.sh
@@ -2,7 +2,7 @@
 
 source $HOME/.setup_sal_env.sh
 
-(ospl start >>/tmp/messages 2>&1 && \
+(ospl start | tee /tmp/messages && \
 sleep 2 && \
 chmod a+rw $(ls -t /tmp/spdds* | head -n 1) && \
 echo "# - ospl daemon ready" ) && \
