7b18d3b436c55f835d80b9e94402b43d85b01b31
Patrick Ingraham <pingraham@lsst.org>
2022-03-09 06:06:53 -0700

    Create docker-compose file and associated setup scripts for each lab-based CSC.

---
+++ tucson-teststand/calibration_lab/setup_dev_electrometer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+echo "Running setup for environment for ts_electrometer. The index {$CSC_INDEX} is defined in the docker-compose file."
+source /home/saluser/.setup_salobj.sh
+setup ts_config_atcalsys -t current
+setup ts_idl -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_xml -t current
+
+echo "# Running custom setups of repos"
+setup -j ts_electrometer -r ~/develop/ts_electrometer
+setup -j ts_config_ocs -r ~/develop/ts_config_ocs
+
+echo '# Installing dependencies and declaring OSPL_URI'
+pip install pyserial  # conda takes too long
+export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
+
+echo "# Launching electrometer with CSC_INDEX=$CSC_INDEX"
+python /home/saluser/develop/ts_electrometer/bin/run_electrometer.py ${CSC_INDEX} 
+#/bin/bash --rcfile /home/saluser/.bashrc
---
+++ tucson-teststand/calibration_lab/setup_dev_fiberspectrograph.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+echo "Running setup for environment for FiberSpectrograph"
+source /home/saluser/.setup_salobj.sh
+
+setup ts_config_ocs -t current
+setup ts_idl -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_xml -t current
+
+echo "# Running custom setups of repos"
+setup -j ts_config_ocs -r ~/develop/ts_config_ocs
+setup -j ts_FiberSpectrograph -r ~/develop/ts_FiberSpectrograph
+
+echo '#Installing dependencies and declaring OSPL_URI'
+export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
+echo 'Setting up LFA'
+export S3_ENDPOINT_URL=https://s3.tu.lsst.org
+source /deploy-lsstts/.lfa_cred.sh
+
+echo "# Launching FiberSpectrograph with CSC_INDEX=$CSC_INDEX"
+python /home/saluser/develop/ts_FiberSpectrograph/bin/run_FiberSpectrograph.py ${CSC_INDEX} 
+#/bin/bash --rcfile /home/saluser/.bashrc
