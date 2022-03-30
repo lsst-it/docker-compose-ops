530fceade9bc09dd15c16a2e91b713e628abcf08
Patrick Ingraham <pingraham@lsst.org>
2022-03-30 14:28:37 -0700

    Update calibration_lab docker-compose and support scripts for c25

--- tucson-teststand/calibration_lab/setup_dev_atmonochromator.sh
+++ tucson-teststand/calibration_lab/setup_dev_atmonochromator.sh
@@ -19,9 +19,6 @@ setup -j ts_idl -r ~/develop/ts_idl
 
 export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
 
-echo "Building new idl's due to enumeration change"
-make_idl_files.py ATMonochromator
-
 echo "# Launching atmonochromator CSC"
 python /home/saluser/develop/ts_atmonochromator/bin/atmonochromator_csc.py
 #/bin/bash --rcfile /home/saluser/.bashrc
--- tucson-teststand/calibration_lab/setup_dev_linearstage.sh
+++ tucson-teststand/calibration_lab/setup_dev_linearstage.sh
@@ -16,26 +16,17 @@ setup ts_salobj -t current
 setup ts_standardscripts -t current
 setup ts_xml -t current
 
-echo "# Setting up sal, salobj and scriptqueue"
+echo "Setting up sal, salobj and scriptqueue"
 
-#setup ts_xml -t current
-#setup -j ts_xml -r ~/develop/ts_xml
-# sets up ts_xml to work for all environments (meaning make_idl_files.py will see it)
-# eups declare -r ~/develop/ts_xml -t $USER
-# setup -j ts_xml -r ~/develop/ts_xml
-#make_idl_files LinearStage
-#setup ts_xml -t $USER
-# isn't working, have to manually run `setup -r .` in the ts_xml directory
-
-echo "# Running custom setups of repos"
+echo "Running custom setups of repos"
 
 setup -j ts_config_mtcalsys -r ~/develop/ts_config_mtcalsys
 setup -j ts_LinearStage -r ~/develop/ts_LinearStage
 
-echo '#Installing dependencies and declaring OSPL_URI'
+echo 'Installing dependencies and declaring OSPL_URI'
 pip install develop/ts_LinearStage/.[dev]
 export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
 
-echo '# now exec into the container to make new xml change, make idl file for the LinearStage, then launch the container"
-
+echo "# Launching ts_LinearStage with CSC_INDEX=$CSC_INDEX"
+python /home/saluser/develop/ts_LinearStage/bin/run_linearstage_csc.py ${CSC_INDEX} 
 /bin/bash --rcfile /home/saluser/.bashrc
