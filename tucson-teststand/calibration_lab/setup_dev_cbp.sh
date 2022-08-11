d8fb786edebd320cc0de458905c82d9985c04a26
Patrick Ingraham <pingraham@lsst.org>
2022-08-11 11:37:51 -0700

    Add support for CBP

---
+++ tucson-teststand/calibration_lab/setup_dev_cbp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+echo "Running setup for environment for ts_CBP CSC"
+source /home/saluser/.setup_salobj.sh
+
+setup ts_config_mtcalsys -t current
+setup ts_idl -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_tcpip -t current
+
+echo "Running custom setups of repos"
+setup -j ts_config_mtcalsys -r ~/develop/ts_config_mtcalsys
+setup -j ts_cbp -r ~/develop/ts_CBP
+setup -j ts_simactuators -r ~/develop/ts_simactuators
+
+# echo 'Installing dependencies and declaring OSPL_URI'
+export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
+
+echo "# Launching ts_CBP CSC"
+python /home/saluser/develop/ts_CBP/bin/run_cbp
+# /bin/bash --rcfile /home/saluser/.bashrc
