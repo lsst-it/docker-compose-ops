2dcabfbfac4e0b373e617edc3fae59c1e701e792
Michael Reuter <mareuter@lsst.org>
2023-02-03 12:35:26 -0700

    Address reviewer comment.

--- summit/auxtel-ill-control/setup_dev_fiberspectrograph.sh
+++ summit/auxtel-ill-control/setup_dev_fiberspectrograph.sh
@@ -15,11 +15,11 @@ setup -j ts_config_ocs -r ~/develop/ts_config_ocs
 setup -j ts_fiberspectrograph -r ~/develop/ts_FiberSpectrograph
 setup -j ts_utils -r ~/develop/ts_utils
 
-echo '#Installing dependencies and declaring OSPL_URI'
+echo '# Installing dependencies and declaring OSPL_URI'
 export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
 echo 'Setting up LFA'
 export S3_ENDPOINT_URL=https://s3.cp.lsst.org
 
 echo "# Launching FiberSpectrograph with CSC_INDEX=$CSC_INDEX"
-python /home/saluser/develop/ts_FiberSpectrograph/bin/run_fiberspectrograph ${CSC_INDEX} 
+python /home/saluser/develop/ts_FiberSpectrograph/bin/run_fiberspectrograph ${CSC_INDEX}
 #/bin/bash --rcfile /home/saluser/.bashrc
